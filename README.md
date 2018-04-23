58869b712a74fb6c6c1f08e1be57d29c3f236cde
opeide <fredrikopeide@gmail.com>
2018-04-23 21:19:01 +0200

    terminal callable. norm distace measure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Recreate a target photo with a mosaic of other photos
+Recreate a target photo with a mosaic of other photos.
+
+Example use: 
+python3 dev/photomosaic/photomosaic.py --source ~/Downloads/flower_photos/sunflowers/ --target ~/dev/photomosaic/test_data/target.jpg --save_path ~/dev/photomosaic/test_data/result.jpg --tile_size 16
+
--- photomosaic.py
+++ photomosaic.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 __author__ = "Fredrik Opeide"
 
 import numpy as np
@@ -5,9 +6,12 @@ import glob
 import matplotlib.pyplot as plt
 from PIL import Image
 import time
+import sys
+import getopt
 
 
 _tile_histograms = []
+_tile_flats = []
 
 
 def create_mosaic(path_tiles, path_target, tile_size):
@@ -52,21 +56,37 @@ def add_tile_to_image(img, tile, tile_location):
     return img
 
 
-def get_best_match(target, source):
-    if not _tile_histograms:    #simple dynamic programming
-        for img in source:
-            hist, _ = np.histogram(img.flatten(), bins=32, range=[0, 255])
-            _tile_histograms.append([img, hist])
-    hist_target, _ = np.histogram(target.flatten(), bins=32, range=[0, 255])
+def get_best_match(target, source, loss_type='norm'):
+    if loss_type == 'norm':
+        if not _tile_flats:    #simple dynamic programming
+            for img in source:
+                flat = img.flatten()/255.0
+                _tile_flats.append([img, flat])
+        flat_target = target.flatten()/255.0
+        lowest_diff = 999.0
+        closest_img = None
+        for img, flat_img in _tile_flats:
+            diff = np.linalg.norm(flat_target-flat_img)
+            if diff < lowest_diff:
+                lowest_diff = diff
+                closest_img = img
+        return closest_img
 
-    best_similarity = -1
-    closest_img = None
-    for img, hist_img in _tile_histograms:
-        similarity = histogram_intersection(hist_target, hist_img)
-        if similarity > best_similarity:
-            best_similarity = similarity
-            closest_img = img
-    return closest_img
+    elif loss_type == 'histogram':
+        if not _tile_histograms:    #simple dynamic programming
+            for img in source:
+                hist, _ = np.histogram(img.flatten(), bins=32, range=[0, 255])
+                _tile_histograms.append([img, hist])
+        hist_target, _ = np.histogram(target.flatten(), bins=32, range=[0, 255])
+
+        best_similarity = -1
+        closest_img = None
+        for img, hist_img in _tile_histograms:
+            similarity = histogram_intersection(hist_target, hist_img)
+            if similarity > best_similarity:
+                best_similarity = similarity
+                closest_img = img
+        return closest_img
 
 
 def histogram_intersection(h1, h2):
@@ -99,12 +119,27 @@ def show_image(img):
     plt.show()
 
 
-#TODO: make callablle from terminal
 if __name__ == '__main__':
-    path_tiles = '/home/opeide/Downloads/flower_photos/dandelion/'
-    path_target = 'test_data/target.jpg'
-    path_result = '/home/opeide/dev/photomosaic/result.png'
-    tile_size = [16, 16]
+    long_args = ['source=', 'target=', 'save_path=', 'tile_size=']
+    opts, _ = getopt.getopt(sys.argv[1:], '', long_args)
+
+    missing_args = ['--source', '--target', '--save_path', '--tile_size']
+    received_args = {}
+    for arg, val in opts:
+        try:
+            received_args[arg] = val
+            missing_args.remove(arg)
+        except:
+            raise Exception('Unexpected argument {}, expecting: {}'.format(arg, long_args))
+    if missing_args:
+        raise Exception('Missing argument: {}'.format(missing_args))
+
+    print(received_args)
+    path_tiles = received_args['--source']
+    path_target = received_args['--target']
+    path_result = received_args['--save_path']
+    n = int(received_args['--tile_size'])
+    tile_size = [n, n]
 
     mosaic = create_mosaic(path_tiles, path_target, tile_size)
     show_image(mosaic)
